3caed0a2af32606b78095cae3f50f8838c2a0604
kiaksarg <kiaksarg@gmail.com>
2016-05-27 23:54:45 +0430

    Add Past Tool Strip Menu Item & Bug fixes

--- FormattedText/About.designer.cs
+++ FormattedText/About.designer.cs
@@ -57,10 +57,10 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(54, 63);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(95, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(187, 13);
             this.linkLabel1.TabIndex = 8;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "www.Introducing.ir";
+            this.linkLabel1.Text = " http://introducing.ir/1395/03/07/507";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // linkLabel2
